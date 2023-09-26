9e1c675ad6735ff414aa94d42d19bac93e5369b0
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2023-09-25 20:12:07 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,30 +24,30 @@ author = "Roger Hatfull"
 # -- General configuration ---------------------------------------------------
 # -- General configuration
 
-extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-]
+#extensions = [
+#    "sphinx.ext.duration",
+#    "sphinx.ext.doctest",
+#    "sphinx.ext.autodoc",
+#    "sphinx.ext.autosummary",
+#    "sphinx.ext.intersphinx",
+#]
 
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
+#intersphinx_mapping = {
+#    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+#    "python": ("https://docs.python.org/3/", None),
+#    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+#}
+#intersphinx_disabled_domains = ["std"]
 
-templates_path = ["_templates"]
+#templates_path = ["_templates"]
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
+#epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+#exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
