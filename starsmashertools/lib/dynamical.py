67f75c8f5d836cf8519844d3797138aedbc59666
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2023-10-12 22:20:14 -0600

    hotfix

--- starsmashertools/lib/dynamical.py
+++ starsmashertools/lib/dynamical.py
@@ -1,11 +1,11 @@
-import starsmashertools.lib.simulation as simulation
-import starsmashertools.lib.binary as binary
+import starsmashertools.lib.simulation
+import starsmashertools.lib.binary
 import starsmashertools.helpers.path as path
 import starsmashertools.preferences as preferences
 from starsmashertools.helpers.apidecorator import api
 
 
-class Dynamical(simulation.Simulation, object):
+class Dynamical(starsmashertools.lib.simulation.Simulation, object):
     def _get_children(self, *args, **kwargs):
         search_directory = kwargs.get('search_directory', preferences.get_default('Simulation', 'search directory'))
         search_directory = path.realpath(search_directory)
@@ -13,7 +13,7 @@ class Dynamical(simulation.Simulation, object):
         restartradfile = self.get_initialfile()
         duplicate = path.find_duplicate_file(restartradfile, search_directory, throw_error=True)
 
-        return [binary.Binary(path.dirname(duplicate))]
+        return [starsmashertools.lib.binary.Binary(path.dirname(duplicate))]
 
     @api
     def get_initialfile(self):
