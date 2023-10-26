6cd148b006c8382f5309d1e659d796dcdba22b65
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2023-10-25 22:49:40 -0600

    maybe?

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'starsmashertools', 'lib', 'simulation')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'starsmashertools')))
 
 #import numpy
 #import starsmashertools
