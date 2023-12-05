0589ad9771ac2c9c465fd66e944d573923f924ef
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2023-12-04 20:47:41 -0700

    hotfix

--- starsmashertools/lib/simulation.py
+++ starsmashertools/lib/simulation.py
@@ -367,7 +367,9 @@ class Simulation(object):
             t = initial_output['t']
         except starsmashertools.lib.output.Reader.CorruptedFileError as e:
             import warnings
-            warnings.warn(str(e)+"\nUsing the first output file instead of the restartrad file", skip_file_prefixes=(starsmashertools.helpers.path.dirname(__file__),))
+            message = str(e)+"\nUsing the first output file instead of the restartrad file"
+            warnings.warn_explicit(message, UserWarning, __file__, None, module=starsmashertools)
+            #warnings.warn(, skip_file_prefixes=(starsmashertools.helpers.path.dirname(__file__),))
             # If the restartrad file is corrupted, try to access just the very
             # first output file
             initial_output = self.get_output(0)
