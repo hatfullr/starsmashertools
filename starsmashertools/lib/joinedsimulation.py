ade038dfa7260fe9742632a879376d0fbb1a79af
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2024-02-28 23:21:02 -0700

    hotfix?

--- starsmashertools/lib/joinedsimulation.py
+++ starsmashertools/lib/joinedsimulation.py
@@ -21,7 +21,7 @@ class JoinedSimulation(starsmashertools.lib.simulation.Simulation, object):
         
         for simulation in simulations:
             if isinstance(simulation, str):
-                simulation = starsmashertools.get_simulation(simulation)
+                simulation = starsmashertools.simulation.Simulation(simulation)
             if not isinstance(simulation, starsmashertools.lib.simulation.Simulation):
                 raise TypeError("A JoinedSimulation can only consist of type 'Simulation', not '%s'" % type(simulation).__name__)
 
