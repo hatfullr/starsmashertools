e17e419ac3a8ea1ba5a3c37315f376a093627d70
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2024-03-08 12:59:39 -0700

    hotfix

--- starsmashertools/helpers/string.py
+++ starsmashertools/helpers/string.py
@@ -122,7 +122,7 @@ class ProgressMessage(LoadingMessage, object):
 
     def get_message(self):
         total = float(self.max - self.min)
-        if span == 0: current = 100.
+        if total == 0: current = 100.
         else: current = self.progress / total * 100.
         return self.message + self.suffixes[self._index].format(
             current = self.progress,
