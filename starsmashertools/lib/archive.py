2b70c755cc0589d016fe28dac1fe6af083b8037d
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2024-05-13 23:34:35 -0600

    hotfix?

--- starsmashertools/lib/archive.py
+++ starsmashertools/lib/archive.py
@@ -225,7 +225,7 @@ class ArchiveValue(object):
         if isinstance(value, bytes):
             # Catch values which are already serialized
             try:
-                value = ArchiveValue.deserialize(self.identifier, value)
+                value = ArchiveValue.deserialize(self.identifier, value).value
             except: pass
             
         self.value = value
