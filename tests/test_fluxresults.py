efda99d86554a35a64116c3ad30439023d18a1b2
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2024-05-28 23:15:21 -0600

    update test

--- tests/test_fluxresults.py
+++ tests/test_fluxresults.py
@@ -37,19 +37,105 @@ class TestFluxResults(basetest.BaseTest):
     
     def testSave(self):
         for details in self.fluxresult_details:
-            result = starsmashertools.lib.flux.FluxResult.load(
+            result = starsmashertools.lib.flux.FluxResults.load(
                 os.path.join(test_fluxresult.fluxdir, details['filename']),
             )
+            
+            # Save as a test file
             result.save(TestFluxResults.test_filename)
             self.assertTrue(os.path.exists(TestFluxResults.test_filename))
             os.remove(TestFluxResults.test_filename)
     
+    def testAdd(self):
+        details = test_fluxresult.load_fluxresult_details().fluxresult_details
 
+        total = starsmashertools.lib.flux.FluxResults()
+        for i, detail in enumerate(details):
+            path = os.path.join(test_fluxresult.fluxdir, detail['filename'])
+            result = starsmashertools.lib.flux.FluxResult.load(path)
+            total.add(result)
+            for branch, leaf in total.flowers():
+                self.assertEqual(i + 1, len(leaf))
+        total.save(TestFluxResults.test_filename)
+
+        new = starsmashertools.lib.flux.FluxResults.load(TestFluxResults.test_filename)
+        for branch, leaf in new.flowers():
+            self.assertIn(branch, total.branches())
+            self.assertEqual(leaf, total[branch])
+        for branch, leaf in total.flowers():
+            self.assertIn(branch, new.branches())
+            self.assertEqual(leaf, new[branch])
+
+
+        # Try with a simple, non-nested "allowed" keyword
+        total = starsmashertools.lib.flux.FluxResults(allowed = {'time' : True})
+        for i, detail in enumerate(details):
+            path = os.path.join(test_fluxresult.fluxdir, detail['filename'])
+            result = starsmashertools.lib.flux.FluxResult.load(path)
+            total.add(result)
+            for branch, leaf in total.flowers():
+                self.assertEqual(i + 1, len(leaf))
+            self.assertEqual(1, len(total.branches()))
+
+        # Try with a difficult, nested 'allowed' keyword
+        total = starsmashertools.lib.flux.FluxResults(allowed = {
+            'kwargs' : {
+                'verbose' : True,
+            },
+        })
+        for i, detail in enumerate(details):
+            path = os.path.join(test_fluxresult.fluxdir, detail['filename'])
+            result = starsmashertools.lib.flux.FluxResult.load(path)
+            total.add(result)
+            for branch, leaf in total.flowers():
+                self.assertEqual(i + 1, len(leaf))
+            self.assertEqual(1, len(total.branches()))
+
+        # Try with the most difficult nested 'allowed' keyword
+        total = starsmashertools.lib.flux.FluxResults(allowed = {
+            'simulation' : True,
+            'kwargs' : True,
+        })
+        i = 0
+        for detail in details:
+            path = os.path.join(test_fluxresult.fluxdir, detail['filename'])
+            result = starsmashertools.lib.flux.FluxResult.load(path)
+            total.add(result)
+            i += 1
+            for branch, leaf in total.flowers():
+                self.assertEqual(i, len(leaf))
+            self.assertNotEqual(2, len(total.branches()))
+
+        os.remove(TestFluxResults.test_filename)
+        total.save(TestFluxResults.test_filename)
+
+        # Try add after save
+        for detail in details:
+            path = os.path.join(test_fluxresult.fluxdir, detail['filename'])
+            result = starsmashertools.lib.flux.FluxResult.load(path)
+            total.add(result)
+            i += 1
+            for branch, leaf in total.flowers():
+                self.assertEqual(i, len(leaf))
+
+        os.remove(TestFluxResults.test_filename)
+        total.save(TestFluxResults.test_filename)
+
+        new = starsmashertools.lib.flux.FluxResults.load(TestFluxResults.test_filename)
+        for branch, leaf in new.flowers():
+            self.assertIn(branch, total.branches())
+            self.assertEqual(leaf, total[branch])
+        for branch, leaf in total.flowers():
+            self.assertIn(branch, new.branches())
+            self.assertEqual(leaf, new[branch])
+        
+        
 class TestLoader(unittest.TestLoader, object):
     def getTestCaseNames(self, *args, **kwargs):
         return [
             'testLoad',
             'testSave',
+            'testAdd',
         ]
 
 if __name__ == '__main__':
