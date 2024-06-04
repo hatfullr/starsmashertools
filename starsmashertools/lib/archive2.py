0f932c3633d97fe1b1390e0295f245b40967c5f2
Roger Hatfull <35510106+hatfullr@users.noreply.github.com>
2024-06-03 21:53:35 -0600

    debug

--- starsmashertools/lib/archive2.py
+++ starsmashertools/lib/archive2.py
@@ -59,7 +59,7 @@ class Buffer(object):
 
         self.seek(start)
         left_content = self.read(curpos - start)
-        right_content = self.read(stop - curpos)
+        right_content = self.read(min(stop - curpos, self.size() - curpos))
         first_string = str(left_content + b' ' + right_content)[2:-1]
         bottom_string = '^'*(len(str(left_content)[2:-1])) + '*' + ' '*len(str(right_content)[2:-1]) + '\n' + ' '*len(str(left_content)[2:-1]) + 'pos'
 
